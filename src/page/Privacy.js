9e5f863e9b33824d4b6c75577ea97ffddd612668
Sortoite <zachatatem@gmail.com>
2023-03-03 08:15:24 -0600

    Feat: add router with privacy page

---
+++ src/page/Privacy.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+//import animate on scroll
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+//import components
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Privacy = () => {
+  Aos.init();
+  return (
+    <div className="flex flex-col max-w-xl mx-auto w-full items-center overflow-hidden">
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default Privacy;
