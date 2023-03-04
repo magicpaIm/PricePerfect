da659bcdde1ef418bbfd3965dd7f1e78ca93dbe0
Sortoite <zachatatem@gmail.com>
2023-03-03 21:05:57 -0600

    Feat: send contact message

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,32 @@
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+// const support = "support@priceperfect.io";
+const support = "zachatatem@gmail.com";
+
 const ContactForm = () => {
+  const [status, setStatus] = useState("Submit");
+  const form = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_839tvkd",
+        "template_wsj7vpd",
+        form.current,
+        "WpcdHKXte2Drc_Q_u"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <>
       <div className="bg-white dark:bg-gray-900 " style={{ width: "80%" }}>
@@ -7,7 +35,12 @@ const ContactForm = () => {
             Contact Us
           </h2>
 
-          <form action="#" className="space-y-8 text-xl ">
+          <form
+            ref={form}
+            action="#"
+            className="space-y-8 text-xl"
+            onSubmit={handleSubmit}
+          >
             <div className="pb-10">
               <label
                 for="email"
@@ -18,6 +51,7 @@ const ContactForm = () => {
               <input
                 type="email"
                 id="email"
+                name="email"
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-5  sm:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="name@flowbite.com"
                 required
@@ -33,6 +67,7 @@ const ContactForm = () => {
               <input
                 type="text"
                 id="subject"
+                name="subject"
                 className="block p-5 sm:p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Let us know how we can help you"
                 required
@@ -47,13 +82,14 @@ const ContactForm = () => {
               </label>
               <textarea
                 id="message"
+                name="message"
                 rows="6"
                 className="block p-5 sm:p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Leave a comment..."
               ></textarea>
             </div>
             <button class="bg-blue-500 text-xs lg:text-lg px-1 py-2 lg:px-8 lg:py-3 text-white rounded-lg font-medium w-fit">
-              Send message
+              {status}
             </button>
           </form>
         </div>
